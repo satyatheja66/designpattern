fba1df34f2b9f42f9101e7394efe6094db9064d1
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2021-01-15 20:22:13 +0200

    state solution 1

--- src/state/exercise1/ManagerState.java
+++ src/state/exercise1/ManagerState.java
@@ -3,18 +3,22 @@
  * Dr Heinz Kabutz from JavaSpecialists.eu and may not be
  * distributed without written consent.
  *
- * Copyright 2001-2018, Heinz Kabutz, All rights reserved.
+ * Copyright 2001-2021, Heinz Kabutz, All rights reserved.
  */
+
 package state.exercise1;
 
-public abstract class State {
+public class ManagerState extends State {
     public int pay(Employee employee) {
-        return 0;
+        System.out.println("Paying lots of $$$ to manager");
+        return 30000;
     }
 
     public void advance(Employee employee) {
+        employee.setState(new RetireeState());
     }
 
     public void fire(Employee employee) {
+        employee.setState(new EndState());
     }
 }
--- src/state/exercise1/ProgrammerState.java
+++ src/state/exercise1/ProgrammerState.java
@@ -3,18 +3,22 @@
  * Dr Heinz Kabutz from JavaSpecialists.eu and may not be
  * distributed without written consent.
  *
- * Copyright 2001-2018, Heinz Kabutz, All rights reserved.
+ * Copyright 2001-2021, Heinz Kabutz, All rights reserved.
  */
+
 package state.exercise1;
 
-public abstract class State {
+public class ProgrammerState extends State {
     public int pay(Employee employee) {
-        return 0;
+        System.out.println("Programmer getting paid");
+        return 3000;
     }
 
     public void advance(Employee employee) {
+        employee.setState(new ManagerState());
     }
 
     public void fire(Employee employee) {
+        employee.setState(new EndState());
     }
 }
