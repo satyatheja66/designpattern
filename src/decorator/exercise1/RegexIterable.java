bc19779e919c406ae67eaeb585aaf3422564ce75
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2021-01-15 19:40:41 +0200

    Changed Regex to create iterator from stream

--- src/decorator/exercise1/RegexIterable.java
+++ src/decorator/exercise1/RegexIterable.java
@@ -8,22 +8,29 @@
 package decorator.exercise1;
 
 import java.util.*;
+import java.util.regex.*;
 import java.util.stream.*;
 
 public class RegexIterable<T> implements Iterable<T> {
-    private final List<T> filteredList;
+    private final Iterable<T> it;
+    private final Pattern pattern;
 
     // at construction, we build up a new list and add all those
     // objects whose toString() method matches the regular expression
     // Our iterator then simply walks over that list.  remove() should not be
     // allowed
     public RegexIterable(Iterable<T> it, String regex) {
-        filteredList = StreamSupport.stream(it.spliterator(), false)
-            .filter(obj -> String.valueOf(obj).matches(regex))
-            .collect(Collectors.toUnmodifiableList());
+        this.it = it;
+        pattern = Pattern.compile(regex);
+    }
+
+    private boolean matches(String input) {
+        return pattern.matcher(input).matches();
     }
 
     public Iterator<T> iterator() {
-        return filteredList.iterator();
+        return StreamSupport.stream(it.spliterator(), false)
+            .filter(obj -> matches(String.valueOf(obj)))
+            .iterator();
     }
 }
